56b8ac8fd05512b0648f4fd68f8728d696b117ac
elecmastsv-arch <elecmast.sv@gmail.com>
2025-11-03 18:22:23 -0300

    Implement view mode toggle in main.jsx
    Added conditional rendering for view mode based on URL parameters.

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,25 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AdminApp from './AdminApp.jsx'
-createRoot(document.getElementById('root')).render(<React.StrictMode><AdminApp/></React.StrictMode>)
+import App from './App.jsx'
+
+// Determinar si estamos en modo visor basado en los parámetros de la URL
+const urlParams = new URLSearchParams(window.location.search)
+const isViewMode = urlParams.has('view') || urlParams.get('mode') === 'view'
+const tournamentSlug = urlParams.get('t')
+
+// Si estamos en modo visor y hay un slug de torneo, mostramos directamente la vista pública
+// De lo contrario, mostramos la aplicación de administrador normal
+const Root = () => {
+  if (isViewMode && tournamentSlug) {
+    return <App initialViewMode={true} viewSlug={tournamentSlug} />
+  } else {
+    return <AdminApp />
+  }
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+)
